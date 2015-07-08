368b3ab83b8e86f8afe1616571d5127a8c3a136e
Jan Suchotzki <jan@suchotzki.de>
2015-07-08 23:41:46 +0200

    Vereinfacht das Programm indem auf die Verwendung von TPL verzichtet wird.

--- AsyncStreamReader/Programm.cs
+++ AsyncStreamReader/Programm.cs
@@ -6,26 +6,9 @@ using System.Threading.Tasks;
 
 class Program {
   static void Main() {
-  	// um einen Task abbrechen zu können, brauchst du einen Token
-  	CancellationTokenSource cTokenSource = new CancellationTokenSource();
-  	CancellationToken cToken = cTokenSource.Token;
-
-  	// nun startest du die Tasks. Den den du abbrechen willst, gibst du den Token mit
   	Task webTask = GetWebContentAsync();
-  	Task indicateProgressTask = Task.Run(() => IndicateProgress(cToken), cToken);
 
-  	// hier wartest du, bis der arbeitende Task fertig ist und dann wird der progress Task 
-  	// abgebrochen
-  	webTask.Wait();
-  	cTokenSource.Cancel();
-
-  	// Schließlich wartest du noch, dass auch der progress Task beendet ist.
-  	indicateProgressTask.Wait();
-  }
-
-  static void IndicateProgress(CancellationToken ct)
-  {
-  	while(!ct.IsCancellationRequested)
+  	while(!webTask.IsCompleted)
   	{
   		Console.Write(";o ");
   		Thread.Sleep(100);
