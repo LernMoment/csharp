c740f729d3e3c488a29472c774dfa5a6dc7731c3
Jan Suchotzki <jan@suchotzki.de>
2015-07-09 00:02:38 +0200

    Passt die Formatierung an.

--- AsyncStreamReader/Programm.cs
+++ AsyncStreamReader/Programm.cs
@@ -5,36 +5,35 @@ using System.Threading;
 using System.Threading.Tasks;
 
 class Program {
-  static void Main() {
-  	Task webTask = GetWebContentAsync();
+	static void Main() {
+		Task webTask = GetWebContentAsync();
 
-  	while(!webTask.IsCompleted)
-  	{
-  		Console.Write(";o ");
-  		Thread.Sleep(100);
-  	}
+		while(!webTask.IsCompleted)
+		{
+			Console.Write(";o ");
+			Thread.Sleep(100);
+		}
 
-  	Console.WriteLine("FERTIG!");
-  }
+		Console.WriteLine("FERTIG!");
+	}
 
-  static async Task GetWebContentAsync()
-  {
-	WebClient myClient = new WebClient();
-	Stream response = await myClient.OpenReadTaskAsync("http://www.contoso.com/index.htm");
-	
-	using (var reader = new StreamReader(response))
+	static async Task GetWebContentAsync()
 	{
-		string line;
-		while((line = await reader.ReadLineAsync()) != null)
-        {
-        	Console.WriteLine();
-            Console.WriteLine(line);
-        }
+		WebClient myClient = new WebClient();
+		Stream response = await myClient.OpenReadTaskAsync("http://www.contoso.com/index.htm");
+	
+		using (var reader = new StreamReader(response))
+		{
+			string line;
+			while((line = await reader.ReadLineAsync()) != null)
+			{
+				Console.WriteLine();
+				Console.WriteLine(line);
+			}
 
-        reader.Close();
-    }
+			reader.Close();
+		}
 
-	response.Close();
-  }
-
+		response.Close();
+	}
 }
