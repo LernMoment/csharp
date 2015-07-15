acb98cc8ff0e83c02fb20bada9be9620af776343
Jan Suchotzki <jan@suchotzki.de>
2015-07-15 11:08:18 +0200

    Erstellt Beispielprojekt fuer LM NuGet.

---
+++ Nuget/Entwicklungsprojekt/LernMoment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EntwicklungsProjekt
+{
+    class LernMoment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wir brauchen noch eine Idee was wir hier tun wollen!");
+
+            Console.WriteLine("Return zum beenden!");
+            Console.ReadLine();
+        }
+    }
+}
